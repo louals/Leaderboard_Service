a00bb2e1cc7316e2b90ef613bffac62db96ef97f
Alsabbagh Louai <144901821+louals@users.noreply.github.com>
2025-07-16 20:46:54 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.routes.leaderboard import router as leaderboard_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()  # Charge les variables d'environnement depuis .env
 
@@ -9,6 +10,16 @@ app = FastAPI(
     description="Microservice qui expose le top 10 des utilisateurs selon leur score",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or "*" to allow all for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 # Inclusion de la route /leaderboard
 app.include_router(leaderboard_router)
